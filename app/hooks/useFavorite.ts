a737b09707b393369a8cfdd702d2366d28fe6840
Chaerul Umam <chaerulumamb244@gmail.com>
2023-05-01 14:17:32 +0700

    modify message

--- app/hooks/useFavorite.ts
+++ app/hooks/useFavorite.ts
@@ -40,7 +40,7 @@ const useFavorite = ({ listingId, currentUser }: IUseFavorite) => {
 
         await request();
         router.refresh();
-        toast.success("Success!");
+        toast.success("Added to favorites");
       } catch (error) {
         toast.error("Something went wrong..");
       }
